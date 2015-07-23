d08fb4fa46e4498f0c2220363699671e5d5aeed0
Ken Moffett <kwmoffett@gmail.com>
2015-07-23 14:53:54 -0500

    Take Two!

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,12 @@
-## Script to Clean Samsung Galaxy S Phone Data
+## Script to Perform Course Project for Getting and Cleaning Data
+
+## This Script does all of the following:
+
+## 1. Merges the training and the test sets to create one data set.
+## 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+## 3. Uses descriptive activity names to name the activities in the data set
+## 4. Appropriately labels the data set with descriptive variable names. 
+## 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 ## Change Working Directory to Location of Data Before Executing Script
 
